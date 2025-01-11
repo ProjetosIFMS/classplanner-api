f6312d7f638615e76ee17044d0dc84c54d4e2a72
VitorSLima <dev.vitorlima@gmail.com>
2025-01-11 14:20:14 -0400

    feat: implement Google authentication strategy with user creation

---
+++ src/strategies/google.strategy.ts
@@ -0,0 +1,46 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { config } from 'dotenv';
+import { Injectable } from '@nestjs/common';
+import { CreateUserRepository } from '../modules/user/repository/create-user.repository';
+import { FindUserByEmailRepository } from '../modules/user/repository/find-user-by-email.repository';
+
+config();
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(
+    private readonly createUserRepository: CreateUserRepository,
+    private readonly findUserByEmailRepository: FindUserByEmailRepository,
+  ) {
+    super({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const { name, emails, photos } = profile;
+    const email = emails[0].value;
+    let user = await this.findUserByEmailRepository.findUserByEmail(email);
+
+    if (!user) {
+      user = await this.createUserRepository.createUser({
+        email,
+        firstName: name.givenName,
+        lastName: name.familyName,
+        picture: photos[0].value,
+        role: 'PROFESSOR',
+      });
+    }
+
+    done(null, user);
+  }
+}
