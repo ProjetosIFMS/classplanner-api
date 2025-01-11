26f67e6827d9b4307a66c000e6ac9704385b31e6
VitorSLima <dev.vitorlima@gmail.com>
2025-01-11 14:23:34 -0400

    feat: implement User module with controller and service for user management

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,45 @@
+import { Prisma } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
+import {
+  CreateUserUseCase,
+  DeleteUserUseCase,
+  FindAllUsersUseCase,
+  FindUserByEmailUseCase,
+  FindUserByIdUseCase,
+  UpdateUserUseCase,
+} from './use-cases';
+
+@Injectable()
+export class UserService {
+  constructor(
+    private readonly createUserUseCase: CreateUserUseCase,
+    private readonly findUserByEmailUseCase: FindUserByEmailUseCase,
+    private readonly findUserByIdUseCase: FindUserByIdUseCase,
+    private readonly deleteUserUseCase: DeleteUserUseCase,
+    private readonly updateUserUseCase: UpdateUserUseCase,
+    private readonly findAllUsersUseCase: FindAllUsersUseCase,
+  ) {}
+  async createUser(data: Prisma.UserCreateInput) {
+    return await this.createUserUseCase.execute(data);
+  }
+
+  async FindUserByEmail(email: string) {
+    return await this.findUserByEmailUseCase.execute(email);
+  }
+
+  async findAll() {
+    return await this.findAllUsersUseCase.execute();
+  }
+
+  async findUserById(id: string) {
+    return await this.findUserByIdUseCase.execute(id);
+  }
+
+  async updateUser(id: string, data: Prisma.UserUpdateInput) {
+    return await this.updateUserUseCase.execute(id, data);
+  }
+
+  async deleteUser(id: string) {
+    return await this.deleteUserUseCase.execute(id);
+  }
+}
