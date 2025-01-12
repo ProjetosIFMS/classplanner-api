c1e56659140bd8fdbbcb02b9ae8ead576cd471eb
VitorSLima <dev.vitorlima@gmail.com>
2025-01-12 15:48:25 -0400

    feat: add PivotModule to app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { DisciplineModule } from './modules/discipline/discipline.module';
+import { PivotModule } from './modules/pivot/pivot.module';
 
 @Module({
-  imports: [AuthModule, DisciplineModule],
+  imports: [AuthModule, DisciplineModule, PivotModule],
   controllers: [AppController],
   providers: [AppService],
 })
