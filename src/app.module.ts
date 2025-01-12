ef75ee43fb6f0b57edf7907c95d20bc92b7c8b92
Vitor Lima <54460782+VitorSLima@users.noreply.github.com>
2025-01-12 18:28:28 -0400

    Revert "Feat/add jwt"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './modules/auth/auth.module';
-import { DisciplineModule } from './modules/discipline/discipline.module';
 
 @Module({
-  imports: [AuthModule, DisciplineModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/user/dto/Role.ts
+++
@@ -1,5 +0,0 @@
-export enum Role {
-  ADMIN = 'ADMIN',
-  PROFESSOR = 'PROFESSOR',
-  COORDINATOR = 'COORDINATOR',
-}
