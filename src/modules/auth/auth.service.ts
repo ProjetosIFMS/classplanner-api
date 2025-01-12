efb3e5218e9163e28d2d205687ef94e541024a1e
VitorSLima <dev.vitorlima@gmail.com>
2025-01-12 16:21:54 -0400

    feat: implement JWT token generation in auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  generateJwt(user: any) {
+    const payload = { username: user.email, sub: user.id };
+    return this.jwtService.sign(payload);
+  }
+
   googleLogin(req) {
     if (!req.user) {
       return 'No user from google';
     }
 
+    const payload = {
+      username: req.user.email,
+      sub: req.user.id,
+    };
+
     return {
       message: 'User information from google',
       user: req.user,
+      acess_token: this.jwtService.sign(payload),
     };
   }
 }
