ab1442b6d62ebeda180b3cc7fefbef33669e22d5
ProjetosIFMS <projetosifmstl@gmail.com>
2025-01-13 11:00:40 -0400

    feat: add course module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { DisciplineModule } from './modules/discipline/discipline.module';
 import { PedagogicalProjectModule } from './modules/pedagogical-project/pedagogical-project.module';
+import { CourseModule } from './modules/course/course.module';
 
 @Module({
-  imports: [AuthModule, DisciplineModule, PedagogicalProjectModule],
+  imports: [
+    AuthModule,
+    DisciplineModule,
+    PedagogicalProjectModule,
+    CourseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
