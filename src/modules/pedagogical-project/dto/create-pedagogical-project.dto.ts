e16cced731652f79936816f42339779a88ace278
MisaelHenrique <misaelhenriquesilva@gmail.com>
2025-01-13 01:17:23 -0400

    feat: add DTOs for creating and updating pedagogical projects

---
+++ src/modules/pedagogical-project/dto/create-pedagogical-project.dto.ts
@@ -0,0 +1,18 @@
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreatePedagogicalProjectDto {
+  @IsString()
+  id?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  course_id: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  year: number;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  status: boolean;
+}
