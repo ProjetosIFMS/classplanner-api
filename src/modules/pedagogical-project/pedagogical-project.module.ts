aee1eb75d32087e6bd7785c7ed91803e7bf49c73
MisaelHenrique <misaelhenriquesilva@gmail.com>
2025-01-13 09:10:49 -0400

    feat: update Pedagogical Project module to include missing repositories and use cases

--- src/modules/pedagogical-project/pedagogical-project.module.ts
+++ src/modules/pedagogical-project/pedagogical-project.module.ts
@@ -1,21 +1,25 @@
 import { Logger, Module } from '@nestjs/common';
-// import * as UseCases from './use-cases';
+import * as UseCases from './use-cases';
 import { DeletePedagogicalProjectRepository } from './repository/delete-pedagogical-project.repository';
 import { PedagogicalProjectController } from './pedagogical-project.controller';
 import { CreatePedagogicalProjectRepository } from './repository/create-pedagogical-project.repository';
 import { FindPedagogicalProjectByIdRepository } from './repository/find-pedagogical-project-by-id.repository';
-import { UpdateAreaRepository } from '../area/repository/update-area.repository';
 import { PedagogicalProjectService } from './pedagogical-project.service';
+import { FindAllPedagogicalProjectsRepository } from './repository/find-all-pedagogical-project.repository';
+import { UpdatePedagogicalProjectRepository } from './repository/update-pedagogical-project.repository';
+
+const usecases = Object.values(UseCases);
 
 @Module({
   controllers: [PedagogicalProjectController],
   providers: [
     CreatePedagogicalProjectRepository,
     FindPedagogicalProjectByIdRepository,
+    FindAllPedagogicalProjectsRepository,
     DeletePedagogicalProjectRepository,
-    UpdateAreaRepository,
+    UpdatePedagogicalProjectRepository,
     PedagogicalProjectService,
-    // ...usecases,
+    ...usecases,
     Logger,
   ],
 })
