80ffd81e8b73f43fc05479b61341505236021ad5
VitorSLima <dev.vitorlima@gmail.com>
2025-01-13 23:39:31 -0400

    feat: add Google access token validation and logging to AuthService

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,13 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import axios from 'axios';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly logger: Logger,
+  ) {}
 
   generateJwt(user: any) {
     const payload = { username: user.email, sub: user.id };
     return this.jwtService.sign(payload);
+  }
+
+  async validateGoogleAccessToken(accessToken: string): Promise<any> {
+    const googleUserInfoUrl = process.env.GOOGLE_USERINFO_URL;
+
+    try {
+      const response = await axios.get(
+        `${googleUserInfoUrl}?access_token=${accessToken}`,
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        },
+      );
+
+      return response.data;
+    } catch (error) {
+      this.logger.error('Error validating Google access token', error);
+      throw new UnauthorizedException('Invalid Google access token');
+    }
   }
 
   googleLogin(req) {
