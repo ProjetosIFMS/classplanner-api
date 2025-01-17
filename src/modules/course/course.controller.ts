7e849c729412ffcb10cc58dcf696a425d7fe8ad2
ProjetosIFMS <projetosifmstl@gmail.com>
2025-01-17 17:10:48 -0400

    refactor: add role auth in controller

--- src/modules/course/course.controller.ts
+++ src/modules/course/course.controller.ts
@@ -6,14 +6,21 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { RolesGuard } from 'src/guards/roles.guard';
+import { AuthGuard } from '@nestjs/passport';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from '../user/dto/Role';
 
 @ApiTags('Course')
 @Controller('course')
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+@Roles(Role.COORDINATOR)
 export class CourseController {
   constructor(private readonly courseService: CourseService) {}
 
@@ -23,6 +30,7 @@ export class CourseController {
   }
 
   @Get()
+  @Roles(Role.PROFESSOR)
   findAllCourses() {
     return this.courseService.findAllCourses();
   }
