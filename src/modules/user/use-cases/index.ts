9c9a6a8bec4c4ac5ef3469c551018dd9c3d2ee8c
VitorSLima <dev.vitorlima@gmail.com>
2025-01-17 01:11:32 -0400

    feat: export updateUserArea use case in user module

--- src/modules/user/use-cases/index.ts
+++ src/modules/user/use-cases/index.ts
@@ -4,3 +4,4 @@ export * from './find-all-users.use-case';
 export * from './find-user-by-id.use-case';
 export * from './delete-user.use-case';
 export * from './update-user.use-case';
+export * from './update-user-area.use-case';
