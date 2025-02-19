f5b30ea59d87991bc8e97864d91332d081e41ec6
VitorSLima <dev.vitorlima@gmail.com>
2025-02-19 13:48:11 -0400

    feat: create PeriodModule with controller and repository providers

---
+++ src/modules/period/period.module.ts
@@ -0,0 +1,26 @@
+import { CreatePeriodRepository } from './repository/create-period.repository';
+import { Logger, Module } from '@nestjs/common';
+import { PeriodService } from './period.service';
+import { PeriodController } from './period.controller';
+import * as UseCases from './use-cases';
+import { FindAllPeriodsRepository } from './repository/find-all-periods.repository';
+import { FindPeriodByIdRepository } from './repository/find-period-by-id.repository';
+import { DeletePeriodRepository } from './repository/delete-period.repository';
+import { UpdatePeriodRepository } from './repository/update-period.repository';
+
+const usecases = Object.values(UseCases);
+
+@Module({
+  controllers: [PeriodController],
+  providers: [
+    CreatePeriodRepository,
+    FindAllPeriodsRepository,
+    FindPeriodByIdRepository,
+    DeletePeriodRepository,
+    UpdatePeriodRepository,
+    PeriodService,
+    ...usecases,
+    Logger,
+  ],
+})
+export class PeriodModule {}
