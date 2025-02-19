7100bab68dee01ff7bf988ebb862fcb80f21dac4
VitorSLima <dev.vitorlima@gmail.com>
2025-02-19 13:48:43 -0400

    feat: add input types for creating and updating periods

---
+++ src/modules/period/inputs/create-period.input.ts
@@ -0,0 +1,6 @@
+export type CreatePeriodInput = {
+  id: string;
+  name: string;
+  start_date: Date;
+  end_date: Date;
+};
---
+++ src/modules/period/inputs/update-period.input.ts
@@ -0,0 +1,5 @@
+export type UpdatePeriodInput = {
+  name: string;
+  start_date: Date;
+  end_date: Date;
+};
