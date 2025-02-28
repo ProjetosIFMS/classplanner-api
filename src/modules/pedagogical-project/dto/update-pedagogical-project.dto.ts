99be9c879e2b6293b9bf0d892b4e8222080aa167
VitorSLima <dev.vitorlima@gmail.com>
2025-02-28 16:37:56 -0400

    refactor: expand UpdatePedagogicalProjectDto with new fields and validations

--- src/modules/pedagogical-project/dto/update-pedagogical-project.dto.ts
+++ src/modules/pedagogical-project/dto/update-pedagogical-project.dto.ts
@@ -1,3 +1,45 @@
-import { CreatePedagogicalProjectDto } from './create-pedagogical-project.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
-export class UpdatePedagogicalProjectDto extends CreatePedagogicalProjectDto {}
+export class UpdatePedagogicalProjectDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  course_id: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  year: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsBoolean()
+  status: boolean;
+
+  @ApiProperty()
+  @IsBoolean()
+  @IsNotEmpty()
+  hasTCC: boolean;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
+  stageHours: number;
+
+  @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
+  complementaryHours: number;
+}
