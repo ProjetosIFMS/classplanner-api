95ec7c63153057ddddeb58dacdb26aa8c927dd05
Felipe Flamarini <felipeflamarini@hotmail.com>
2025-03-29 15:13:49 -0300

    feat: make dayoff properties optional in update DTO and input

--- src/modules/dayoff/dto/update-dayoff.dto.ts
+++ src/modules/dayoff/dto/update-dayoff.dto.ts
@@ -1,20 +1,25 @@
-import { IsString, IsEnum } from 'class-validator';
+import { IsString, IsEnum, IsOptional } from 'class-validator';
 import { WEEKDAY } from 'src/modules/dayoff/dto/weekday';
 import { DAYOFF_STATUS } from 'src/modules/dayoff/dto/dayoff-status';
 
 export class UpdateDayoffDto {
+  @IsOptional()
   @IsString()
   reason: string;
 
+  @IsOptional()
   @IsString()
   schedule: string;
 
+  @IsOptional()
   @IsString()
   frequency: string;
 
+  @IsOptional()
   @IsEnum(WEEKDAY)
   weekday: WEEKDAY;
 
+  @IsOptional()
   @IsEnum(DAYOFF_STATUS)
   status: DAYOFF_STATUS;
 }
