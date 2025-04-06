bcbaff1aae8f59addbb52207c470b09bf1dc8e33
PsSave <pedrosviki1265@gmail.com>
2025-04-06 17:07:52 -0300

    feat: include User and Discipline relations in area retrieval methods

--- src/modules/area/repository/find-all-area.repository.ts
+++ src/modules/area/repository/find-all-area.repository.ts
@@ -5,7 +5,12 @@ import { PrismaService } from '../../../shared/databases/prisma.database';
 export class FindAllAreasRepository {
   constructor(private readonly prisma: PrismaService) {}
   async findAllAreas() {
-    const areas = await this.prisma.area.findMany();
+    const areas = await this.prisma.area.findMany({
+      include: {
+        User: true,
+        Discipline: true,
+      },
+    });
 
     return areas;
   }
