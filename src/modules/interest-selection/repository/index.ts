11461cc9d0dc84227726f355eed6908295beb9ee
Felipe Flamarini <felipeflamarini@hotmail.com>
2025-04-17 15:30:45 -0300

    feat: implement delete interest selection by ID repository and changed creation logic

--- src/modules/interest-selection/repository/index.ts
+++ src/modules/interest-selection/repository/index.ts
@@ -1,4 +1,5 @@
 export * from './create-interest-selection.repository';
+export * from './delete-interest-selection-by-id.repository';
 export * from './find-all-interest-selections.repository';
 export * from './find-interest-selection-by-id.repository';
 export * from './find-interest-selection-by-professor-id.repository';
--- src/modules/interest-selection/use-cases/create-interest-selection.use-case.ts
+++ src/modules/interest-selection/use-cases/create-interest-selection.use-case.ts
@@ -1,5 +1,4 @@
 import {
-  ConflictException,
   Injectable,
   Logger,
   ServiceUnavailableException,
@@ -7,40 +6,76 @@ import {
 import { CreateInterestSelectionRepository } from '../repository/create-interest-selection.repository';
 import { CreateInterestSelectionDto } from '../dto/create-interest-selection.dto';
 import { FindInterestSelectionByProfessorIdRepository } from '../repository';
+import { DeleteInterestSelectionById } from 'src/modules/interest-selection/repository/delete-interest-selection-by-id.repository';
+import { ProfessorInterest } from '@prisma/client';
 
 @Injectable()
 export class CreateInterestSelectionUseCase {
   constructor(
     private readonly createInterestSelectionRepository: CreateInterestSelectionRepository,
     private readonly findInterestByProfessorIdRepository: FindInterestSelectionByProfessorIdRepository,
+    private readonly deleteInterestByIdRepository: DeleteInterestSelectionById,
     private readonly logger: Logger,
   ) {}
 
   async execute(user_id: string, data: CreateInterestSelectionDto) {
     try {
-      const relatedInterests =
+      const existingInterests =
         await this.findInterestByProfessorIdRepository.findInterests(user_id);
 
-      if (
-        relatedInterests.some((discipline) =>
-          data.disciplines_ids.includes(discipline.discipline_id),
-        )
-      ) {
-        throw new ConflictException(
-          'Discipline(s) already related to professor',
+      const existingInterestsDisciplinesIds = existingInterests.map(
+        (existingInterest) => existingInterest.discipline_id,
+      );
+
+      const interestsDisciplinesIdsToAdd: string[] =
+        data.disciplines_ids.filter(
+          (discipline_id) =>
+            !existingInterestsDisciplinesIds.includes(discipline_id),
+        );
+
+      const interestsDisciplinesIdsToDelete: string[] =
+        existingInterestsDisciplinesIds.filter(
+          (discipline_id) => !data.disciplines_ids.includes(discipline_id),
+        );
+
+      const returningInterests: ProfessorInterest[] = [];
+
+      if (interestsDisciplinesIdsToAdd.length > 0) {
+        returningInterests.push(
+          ...(await this.createInterestSelectionRepository.createInterestSelection(
+            user_id,
+            {
+              disciplines_ids: interestsDisciplinesIdsToAdd,
+              status: data.status,
+            },
+          )),
         );
       }
 
-      const createdInterest =
-        await this.createInterestSelectionRepository.createInterestSelection(
-          user_id,
-          data,
+      if (interestsDisciplinesIdsToDelete.length > 0) {
+        const deletedInterests = await Promise.all(
+          interestsDisciplinesIdsToDelete.map(async (discipline_id) => {
+            const interestIdToDelete = existingInterests.find(
+              (interest) => interest.discipline_id === discipline_id,
+            )?.id;
+            if (interestIdToDelete) {
+              return await this.deleteInterestByIdRepository.DeleteInterestSelectionById(
+                interestIdToDelete,
+              );
+            }
+            return null;
+          }),
         );
+
+        returningInterests.push(...deletedInterests.filter(Boolean));
+      }
+
       this.logger.log(
         'Interest related to professor',
         CreateInterestSelectionUseCase.name,
       );
-      return createdInterest;
+
+      return returningInterests;
     } catch (err) {
       const error = new ServiceUnavailableException('Something bad happened', {
         cause: err,
